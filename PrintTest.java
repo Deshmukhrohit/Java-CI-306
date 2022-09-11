a2ce3f651b7b953b905e58a9f8d865be37b3da1a
Rohit Deshmukh <87318142+Deshmukhrohit@users.noreply.github.com>
2022-09-11 14:08:30 +0530

    Create PrintTest.java

---
+++ PrintTest.java
@@ -0,0 +1,31 @@
+public class PrintTest
+{
+ private int a;
+ private String b;
+ private double c;
+ public void setA(int a)
+ {
+  this.a=a;
+ }
+ public void setB(String b)
+ {
+  this.b=b;
+ }
+ public void setC(double c)
+ {
+  this.c=c;
+ }
+ public int getA()
+ {
+  return a;
+ }
+ public String getB()
+ {
+  return b;
+ }
+ public double getC()
+ {
+  return c;
+ }
+}
+ 
