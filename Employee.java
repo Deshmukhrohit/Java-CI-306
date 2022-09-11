e3714e31471ebacc2a5b1ecd76d5a49a087d6445
Rohit Deshmukh <87318142+Deshmukhrohit@users.noreply.github.com>
2022-09-11 13:55:47 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,30 @@
+public class Employee
+{
+ private String name;
+ private String designation;
+ private int salary;
+ public void setName(String na)
+ {
+  name = na;
+ }
+ public void setDesignation(String des)
+ {
+  designation = des;
+ }
+ public void setSalary(int s)
+ {
+  salary = s;
+ }
+ public String getName()
+ {
+  return name;
+ }
+ public String getDesignation()
+ {
+ return designation;
+ }
+ public int getSalary()
+ {
+  return salary;
+ }
+}
