ab7dd5a24383bb45997700297bc5b58f001dbf95
Rohit Deshmukh <87318142+Deshmukhrohit@users.noreply.github.com>
2022-09-11 14:10:29 +0530

    Create RunParam.java

---
+++ RunParam.java
@@ -0,0 +1,9 @@
+public class RunParam
+{
+ public static void main(String [] args)
+ {
+  System.out.println("Run time Parameter are ");
+  for(int i=0;i<args.length;i++)
+  System.out.println(args[i]);
+ }
+}
