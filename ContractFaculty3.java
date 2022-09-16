782fb401bbefc2c7076748793bbc563723a9f1d2
Rohit Deshmukh <87318142+Deshmukhrohit@users.noreply.github.com>
2022-09-17 00:34:31 +0530

    Create ContractFaculty3.java

---
+++ ContractFaculty3.java
@@ -0,0 +1,21 @@
+package rohit_project;
+
+public class ContractFaculty3 extends Faculty3{
+	private int contractAmount,contractDuraction;
+	public void setContractAmount(int contractAmount)
+	{
+		this.contractAmount=contractAmount;
+	}
+	public void setContractDuraction(int contractDuraction)
+	{
+		this.contractDuraction=contractDuraction;
+	}
+	public int getContractAmount()
+	{
+		return contractAmount;
+	}
+	public int getContractDuraction()
+	{
+	    return contractDuraction;	
+	}
+}
